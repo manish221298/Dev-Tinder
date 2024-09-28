9f6e87081958751033e1b1977c0bffb0d324e623
manish221298 <iammnsh01@gmail.com>
2024-09-28 19:51:40 -0400

    route changed

--- src/app.js
+++ src/app.js
@@ -3,19 +3,19 @@ const express = require('express')
 const app = express()
 const port = 4001
 
-// app.use("/user", (req, res) => {
+app.use("/user", (req, res) => {
 
-//     const queryData = req.query
-//     res.send({"data is": queryData})
-// })
-
-app.use("/user/:id/:password", (req, res) => {
-
-    const paramsData = req.params
-    console.log(paramsData);
-    
-    res.send({"data is": paramsData})
+    const queryData = req.query
+    res.send({"data is": queryData})
 })
+
+// app.use("/user/:id/:password", (req, res) => {
+
+//     const paramsData = req.params
+//     console.log(paramsData);
+    
+//     res.send({"data is": paramsData})
+// })
 
 
 
