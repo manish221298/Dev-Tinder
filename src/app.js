7defc95934eacd58b0969f5b83da1c24f16ad310
manish221298 <iammnsh01@gmail.com>
2024-09-27 23:09:47 -0400

    dev-tinder initial commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+const port = 4001
+
+app.use("/hello", (req, res) => {
+    res.send("Hello World")
+})
+
+app.use("/users", (req, res) => {
+    res.send("User will get in this route right?")
+})
+
+app.use("/emp", (req, res) => {
+    res.send("Emp will get in this route")
+})
+
+app.listen(port, () =>{
+    console.log("Server is successully listen on port", port);
+})
