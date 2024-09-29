d2acbd289bb12c252f47c55f5390a335f3f4ed7c
manish221298 <iammnsh01@gmail.com>
2024-09-28 20:54:37 -0400

    playing with multiple req-res handler and next()

--- src/app.js
+++ src/app.js
@@ -3,28 +3,23 @@ const express = require('express')
 const app = express()
 const port = 4001
 
-app.use("/user", (req, res) => {
-
-    const queryData = req.query
-    res.send({"data is": queryData})
-})
-
-// app.use("/user/:id/:password", (req, res) => {
-
-//     const paramsData = req.params
-//     console.log(paramsData);
-    
-//     res.send({"data is": paramsData})
-// })
-
-
-
-
-
+// play with multiple req, res handeler
+app.use("/user", (req, res, next) => {
+   next() // called to next req-res handler and return response handler 2
+    res.send("Response handler 1")
+    // next()  // not called next handler if next is there called
+},
+(req, res, next) => {
+    next()
+    res.send("Response handler 2")
+    // next()
+},
+(req, res, next) => {
+    next()
+    res.send("Response handler 3")
+}
+)
 
 app.listen(port, () =>{
     console.log("Server is successully listen on port", port);
 })
-
-
-// xfnbgjcvb
