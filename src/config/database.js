79780e304d08d25ce4ec21b9d4087a4c3372f621
manish221298 <iammnsh01@gmail.com>
2025-02-23 09:28:04 +0530

    add env file

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,21 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   // await mongoose.connect('mongodb://localhost:27017/devTinder')
-  await mongoose.connect(
-    "mongodb+srv://iammnsh01:7HoYuMMrIRlMFCWf@devtinder.gxcuq.mongodb.net/?retryWrites=true&w=majority&appName=DevTinder"
-  );
+
+  await mongoose.connect(process.env.MONGOOSE_SECRET_STRING);
 };
 
 module.exports = connectDB;
+
+// nginx config
+
+// server_name 51.20.122.54;
+
+//     location /api/ {
+//         proxy_pass http://localhost:4001/;  # Pass the request to the Node.js app
+//         proxy_http_version 1.1;
+//         proxy_set_header Upgrade $http_upgrade;
+//         proxy_set_header Connection 'upgrade';
+//         proxy_set_header Host $host;
+//         proxy_cache_bypass $http_upgrade;
+//     }
